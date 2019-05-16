e414e3247f325eaf13ee3fa1dca658b29070cea3
liuqi <a18627830855@163.com>
2019-05-16 18:45:32 +0800

    阳光保险-----实体类-----role------作者:LuKe:fire:

--- src/main/java/com/csd/sunshine/model/entity/Role.java
+++ src/main/java/com/csd/sunshine/model/entity/Role.java
@@ -1,6 +1,9 @@
 package com.csd.sunshine.model.entity;
 
+import lombok.Data;
+
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @Description: sunshine
@@ -8,7 +11,25 @@ import java.io.Serializable;
  * @authod liuqi
  * @date 2019/5/16 18:26
  */
+
+@Data
 public class Role implements Serializable {
 
 
+    private Integer id;
+    private String displayName;//角色展示名
+    private Integer level;      //角色等级
+    private Date createdAt;
+    private Date updatedAt;
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", displayName='" + displayName + '\'' +
+                ", level=" + level +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
 }
